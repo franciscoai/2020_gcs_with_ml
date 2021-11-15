a6e115cf2012a1594b669e0dd9bfba2f96e6092c
rukyas <yasmin12machuca@gmail.com>
2021-11-15 19:14:58 -0300

    ahora sí update

--- code/forwardGCS.py
+++ code/forwardGCS.py
@@ -29,14 +29,13 @@ import pandas as pd
 
 # In[3]:
 
-
 def center_rSun_pixel(headers,plotranges, sat):
     x_cS = (headers[sat]['CRPIX1']*plotranges[sat][sat]*2)/headers[sat]['NAXIS1'] - plotranges[sat][sat]
     y_cS = (headers[sat]['CRPIX2']*plotranges[sat][sat]*2)/headers[sat]['NAXIS2'] - plotranges[sat][sat]
     return x_cS, y_cS
 
 
-# ## Función Kernel Density Estimation
+# ## Función Kernel Density Estimation (se parece a la de intensidad, pero no)
 
 # In[4]:
 
@@ -51,7 +50,7 @@ def xi_yi_zi_KDE(x, y, nbins=300):
      
 
 
-# ## Headers
+# ## Headers de CMEs reales, solo para usar la posición de los satélites
 
 # In[10]:
 
@@ -61,39 +60,29 @@ ISSIflag = False
 
 # Read in your data
 
-#home_Yas
+DATA_PATH = '/gehme/data'
+secchipath = DATA_PATH+'/stereo/secchi/L0'
+lascopath = DATA_PATH+'/soho/lasco/level_1/c2'
 
-CorA =  '/media/yas/DATOS/gehme/2020_gcs_with_ml/data/20101216A/20101216_083900_d4c2A.fts'
-CorB =  '/media/yas/DATOS/gehme/2020_gcs_with_ml/data/20101216B/20101216_083900_d4c2B.fts'
-LascoC2 = '/media/yas/DATOS/gehme/2020_gcs_with_ml/data/20101216/c2/25354898.fts'
-
-
-#DESCOMENTAR PARA USAR EN EL SERVER DESDE ACÁ
-######################################################
-#DATA_PATH = '/gehme/data'
-#secchipath = DATA_PATH+'/stereo/secchi/L0'
-#lascopath = DATA_PATH+'/soho/lasco/level_1/c2'
-
-#CorA = secchipath+'/a/img/cor2/20101214/level1/20101214_162400_04c2A.fts'
-#CorB = secchipath+'/b/img/cor2/20101214/level1/20101214_162400_04c2B.fts'
-#LascoC2 = lascopath+'/20101214/25354684.fts'
-######################################################HASTA ACÁ
+CorA = secchipath+'/a/img/cor2/20101214/level1/20101214_162400_04c2A.fts'
+CorB = secchipath+'/b/img/cor2/20101214/level1/20101214_162400_04c2B.fts'
+LascoC2 = lascopath+'/20101214/25354684.fts'
 
 # STEREO A
 
 ima2, hdra2 = sunpy.io.fits.read(CorA)[0]
 smap_SA2 = sunpy.map.Map(ima2, hdra2)
 
-# # STEREO B
+# STEREO B
 
 imb2, hdrb2 = sunpy.io.fits.read(CorB)[0]
 smap_SB2 = sunpy.map.Map(imb2, hdrb2)
 
 # # LASCO
 
-#if ISSIflag:
+if ISSIflag:
 #     imL1, hdrL1 = sunpy.io.fits.read(fnameL1)[0]
-#    imL2, hdrL2 = sunpy.io.fits.read(LascoC2)[0]
+    imL2, hdrL2 = sunpy.io.fits.read(LascoC2)[0]
     
 #else:
 #     with fits.open(fnameL1) as myfitsL1:
@@ -107,24 +96,24 @@ smap_SB2 = sunpy.map.Map(imb2, hdrb2)
 #         myfitsL1[0].header['HGLT_OBS'] = coordL1ston.lat.deg
 #         myfitsL1[0].header['HGLN_OBS'] = coordL1ston.lon.deg
 #         hdrL1 = myfitsL1[0].header
-#    with fits.open(LascoC2) as myfitsL2:
-#        imL2 = myfitsL2[0].data
-#        myfitsL2[0].header['OBSRVTRY'] = 'SOHO'
-#        coordL2 = get_horizons_coord(-21, datetime.datetime.strptime(myfitsL2[0].header['DATE-OBS'], "%Y-%m-%dT%H:%M:%S.%f"), 'id')
-#        coordL2carr = coordL2.transform_to(sunpy.coordinates.frames.HeliographicCarrington)
-#        coordL2ston = coordL2.transform_to(sunpy.coordinates.frames.HeliographicStonyhurst)
-#        myfitsL2[0].header['CRLT_OBS'] = coordL2carr.lat.deg
-#        myfitsL2[0].header['CRLN_OBS'] = coordL2carr.lon.deg
-#        myfitsL2[0].header['HGLT_OBS'] = coordL2ston.lat.deg
-#        myfitsL2[0].header['HGLN_OBS'] = coordL2ston.lon.deg
-#        hdrL2 = myfitsL2[0].header
+    with fits.open(LascoC2) as myfitsL2:
+        imL2 = myfitsL2[0].data
+        myfitsL2[0].header['OBSRVTRY'] = 'SOHO'
+        coordL2 = get_horizons_coord(-21, datetime.datetime.strptime(myfitsL2[0].header['DATE-OBS'], "%Y-%m-%dT%H:%M:%S.%f"), 'id')
+        coordL2carr = coordL2.transform_to(sunpy.coordinates.frames.HeliographicCarrington)
+        coordL2ston = coordL2.transform_to(sunpy.coordinates.frames.HeliographicStonyhurst)
+        myfitsL2[0].header['CRLT_OBS'] = coordL2carr.lat.deg
+        myfitsL2[0].header['CRLN_OBS'] = coordL2carr.lon.deg
+        myfitsL2[0].header['HGLT_OBS'] = coordL2ston.lat.deg
+        myfitsL2[0].header['HGLN_OBS'] = coordL2ston.lon.deg
+        hdrL2 = myfitsL2[0].header
 # smap_L1 = sunpy.map.Map(imL1, hdrL1)
-##smap_L2 = sunpy.map.Map(imL2, hdrL2)
+smap_L2 = sunpy.map.Map(imL2, hdrL2)
 
-#headers = [hdra2, hdrL2, hdrb2]
+headers = [hdra2, hdrL2, hdrb2]
 
-#prueba sin lasco:
-headers = [hdra2, hdrb2]
+#sin lasco:
+#headers = [hdra2, hdrb2]
 
 
 # ## Tamaño de los occulters referenciados al RSUN
@@ -152,22 +141,20 @@ ks = np.random.random_sample(size = n)
 
 angs = np.random.randint(30, 33, n)
 
-zeros = np.zeros(n)
-
 #cada array de parámetro pasa a ser una columna del set de parámetros:
 
 set_parameters = np.column_stack((CMElons, CMElats, CMEtilts, heights, ks, angs))
 
 
-# ## Función para guardar configuración en CSV
+# ## Save configuración en .CSV
 
 # In[5]:
 
 
-path = '/media/yas/DATOS/gehme/2020_gcs_with_ml/data/forwardGCS_test/'     
+path = '/gehme/projects/2020_gcs_with_ml/data/forwardGCS_test/'     
 date_str = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d_')
 configfile_name = path+date_str+'Set_Parameters.csv'
-header_name = [ 'CMElon',
+header_name = ['CMElon',
            'CMElat',
            'CMEtilt',
            'height',
@@ -181,7 +168,7 @@ set.to_csv(configfile_name)
 
 # In[7]:
 
-def pd_forwardGCS(configfile_name, headers, size_occ=[2,2]):
+def forwardGCS(configfile_name, headers, size_occ=[2,2]):
     # Get the location of sats and the range of each image:
     satpos, plotranges = processHeaders(headers)
     df = pd.DataFrame(pd.read_csv(configfile_name))
@@ -211,10 +198,9 @@ def pd_forwardGCS(configfile_name, headers, size_occ=[2,2]):
             plt.ylim(plotranges[sat][2],plotranges[sat][3])
             plt.axis('off')
             #guardo cada vista CME
-            #home Yas
-            fig.savefig('/media/yas/DATOS/gehme/2020_gcs_with_ml/data/forwardGCS_test/{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_sat{}.png'.format(df['CMElon'][row], df['CMElat'][row], df['CMEtilt'][row], df['height'][row], df['k'][row], df['ang'][row], sat+1),facecolor=fig.get_facecolor())
+
             #en el server:
-            #f.savefig('/gehme/projects/2020_gcs_with_ml/data/forwardGCS_test/{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_sat{}.png'.format(config[0], config[1], config[2], config[3], config[4], config[5], sat+1),facecolor=f.get_facecolor())
+            fig.savefig('/gehme/projects/2020_gcs_with_ml/data/forwardGCS_test/{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_sat{}.png'.format(df['CMElon'][row], df['CMElat'][row], df['CMEtilt'][row], df['height'][row], df['k'][row], df['ang'][row], sat+1),facecolor=fig.get_facecolor())
             #plt.show()
             plt.close(fig)
 
@@ -222,58 +208,9 @@ def pd_forwardGCS(configfile_name, headers, size_occ=[2,2]):
 
 
     
-def forwardGCS(configfile_name, headers, size_occ=[2,2]):
-    
-    # Get the location of sats and the range of each image:
-    satpos, plotranges = processHeaders(headers)
-    
-    
-    # creo Set de imágenes de CMEs para todas las combinaciones de los parámetros de entrada para cada vista de SATPOS:
-    row, column = set_parameters.shape
-    for r in range(row):
-        for c in range(column):
-            
-            clouds = getGCS(set_parameters[r][0], set_parameters[r][1], set_parameters[r][2], set_parameters[r][3],set_parameters[r][4], set_parameters[r][5], satpos)
-            
-           # with open('/gehme/2020_gcs_with_ml/data/test[%f].dat' %r , 'w') as config_file:
-                        
-        #with open('/gehme/projects/2020_gcs_with_ml/data/%s.dat' %config_name, 'a') as config_file:
-            for sat in range(len(satpos)):
-                    #creo figura:
-                    fig = plt.figure(figsize=(3.556,3.556), facecolor='black')
-                    ax = fig.add_subplot()
-                    x = clouds[sat,:,1]
-                    y = clouds[0,:,2]
-                    #calculo el centro y el radio del sol en coordenada de plotrange
-                    x_cS, y_cS= center_rSun_pixel(headers,plotranges, sat)
-                    #mapeo la matriz [zi,xi], con límites [xi,yi]:
-                    # xi, yi, zi = xi_yi_zi_KDE(x, y)
-                    # plt.pcolormesh(xi, yi, zi.reshape(xi.shape), cmap='gist_yarg', facecolor='black')
-                    # plt.imshow(ims[sat], vmin=-10, vmax=10, cmap=cm.binary, zorder=0, extent=plotranges[sat])
-                    #imagen de puntos de la CME:
-                    plt.scatter(clouds[sat,:,1], clouds[0,:,2], s=5, c='purple', linewidths=0)
-                    #circulos occulter y limbo:
-                    occulter = plt.Circle((x_cS, y_cS), size_occ[sat], fc='white')
-                    limbo = plt.Circle((x_cS, y_cS), 1, ec='black', fc='white')
-                    ax.add_artist(occulter)
-                    ax.add_artist(limbo)
-                    #tamaño de la imagen referenciado al tamaño de la imagen del sol:
-                    plt.xlim(plotranges[sat][0],plotranges[sat][1])
-                    plt.ylim(plotranges[sat][2],plotranges[sat][3])
-                    plt.axis('off')
-                    #guardo cada vista CME
-                    #home Yas
-                    fig.savefig('/media/yas/DATOS/gehme/2020_gcs_with_ml/data/forwardGCS_test/{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_sat{}.png'.format(set_parameters[r][0], set_parameters[r][1], set_parameters[r][2], set_parameters[r][3],set_parameters[r][4], set_parameters[r][5], sat+1),facecolor=fig.get_facecolor())
-                    #en el server:
-                    # f.savefig('/gehme/projects/2020_gcs_with_ml/data/forwardGCS_test/{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_sat{}.png'.format(config[0], config[1], config[2], config[3], config[4], config[5], sat+1),facecolor=f.get_facecolor())
-                    #plt.show()
-                    plt.close(fig)
 
 # ## Prueba
 
-#forwardGCS(set_parameters, headers)
-
-pd_forwardGCS(configfile_name, headers)
-
+forwardGCS(configfile_name, headers)
 
 
