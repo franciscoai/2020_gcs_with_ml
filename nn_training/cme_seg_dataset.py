058d5633a9448c657434309f44dccac7d0b7d51b
Florencia Cisterna <f.cisterna@alumno.um.edu.ar>
2023-05-03 11:56:38 -0300

    path update

--- nn_training/cme_seg_dataset.py
+++ nn_training/cme_seg_dataset.py
@@ -40,7 +40,7 @@ def rnd_samples(rng, n):
 #files
 exec_path = os.getcwd()
 DATA_PATH = '/gehme/data'
-OPATH = '/gehme/projects/2020_gcs_with_ml/data/cme_seg_dataset' #'/gehme/projects/2020_gcs_with_ml/data/forwardGCS_test'
+OPATH = '/gehme-gpu/projects/2020_gcs_with_ml/data/cme_seg_dataset' #'/gehme/projects/2020_gcs_with_ml/data/forwardGCS_test'
 
 #sattelite positions
 secchipath = DATA_PATH + '/stereo/secchi/L1'
@@ -56,9 +56,9 @@ ISSIflag = False # flag if using LASCO data from ISSI which has STEREO like head
 par_names = ['CMElon', 'CMElat', 'CMEtilt', 'height', 'k','ang', 'level_cme'] # par names
 par_units = ['deg', 'deg', 'deg', 'Rsun','','deg',''] # par units
 par_rng = [[-180,180],[-70,70],[-90,90],[6,13],[0.25,0.6], [10,60],[5e3,6e3]] # min-max ranges of each parameter in par_names
-par_num = 5  # total number of samples that will be generated for each param
+par_num = 100  # total number of samples that will be generated for each param
 #par_rng = [[165,167],[-22,-20],[-66,-64],[10,15],[0.21,0.23], [19,21],[5e2,1e3]] # example used for script development
-rnd_par=False # set to randomnly shuffle the generated parameters linspace 
+rnd_par=True # set to randomnly shuffle the generated parameters linspace 
 
 # Syntethic image options
 imsize=np.array([512, 512], dtype='int32') # output image size
