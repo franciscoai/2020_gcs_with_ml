0309d4ff396425399ea6e8ac2a0ec1d8478fbc10
Florencia Cisterna <f.cisterna@alumno.um.edu.ar>
2023-06-02 15:40:43 -0300

    updated prep_corona_catalogue.py

--- nn_training/corona_background/prep_corona_catalogue.py
+++ nn_training/corona_background/prep_corona_catalogue.py
@@ -1,7 +1,10 @@
-import pandas as pd
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+from ext_libs.rebin import rebin
 from astropy.io import fits
-
+import pandas as pd
+import glob
 """
 Reads pairs of LVL1 coronograph images from various instruments and saves a differential corona for each pair.
 Images are resized
@@ -12,6 +15,8 @@ lasco_path = exec_path+'/Lista_Final_CMEs_downloads_lascoc2.csv' #'/catalogues/L
 cor2_path = exec_path+'/Lista_Final_CMEs_downloads_cor2.csv'
 lasco_downloads=["pre_a_1h_download_c2","pre_b_1h_download_c2","pre_a_2h_download_c2","pre_b_2h_download_c2"]
 cor2_downloads=["pre_a_1h_download","pre_b_1h_download","pre_a_2h_download","pre_b_2h_download"]
+imsize=[512,512]
+
 
 lasco= pd.read_csv(lasco_path , sep="\t")
 lasco.name='lasco'
@@ -40,17 +45,27 @@ def pathlist(df):
                 element= df.loc[i,j]
                 paths.append(element)
 
-        # for k in range(0,2):        #repetir para evento b
-        #     if (df.loc[i,downloads[k]] != "No data" and df.loc[i,downloads[k]] != "*" and df.loc[i,downloads[k]] != "No img/double data") and (df.loc[i,downloads[k+2]] != "No data" and df.loc[i,downloads[k+2]] != "*" and df.loc[i,downloads[k+2]] != "No img/double data"):
-        #         path_1h=(df.loc[i,downloads[k]])#.replace("level_05","level_1")
-        #         path_2h=(df.loc[i,downloads[k+2]])#.replace("level_05","level_1")
-        #         im1= fits.open(path_1h)
-        #         im2= fits.open(path_2h)
-        #         im=im1[0].data-im2[0].data
-        #         header= fits.getheader(path_1h)
-        #         final_img = fits.PrimaryHDU(im, header=header[0:-3])
+        for k in range(0,2):        #repetir para evento b
+            if (df.loc[i,downloads[k]] != "No data" and df.loc[i,downloads[k]] != "*" and df.loc[i,downloads[k]] != "No img/double data") and (df.loc[i,downloads[k+2]] != "No data" and df.loc[i,downloads[k+2]] != "*" and df.loc[i,downloads[k+2]] != "No img/double data"): 
+                try:
+                        file1=glob.glob(df.loc[i,downloads[k]][0:-5]+"*")
+                        file2=glob.glob(df.loc[i,downloads[k+2]][0:-5]+"*")
+                        path_1h=(file1[0])#.replace("level_05","level_1")
+                        path_2h=(file2[0])#.replace("level_05","level_1")
+                        im1= fits.open(path_1h)
+                        im2= fits.open(path_2h)
+                        image1 = rebin(im1[0].data,imsize,operation='mean') 
+                        image2 = rebin(im2[0].data,imsize,operation='mean') 
+                        im=image1-image2
+                        header= fits.getheader(path_1h)
+                        header['NAXIS1'] = imsize[0]   
+                        header['NAXIS2'] = imsize[1]
+                        final_img = fits.PrimaryHDU(im, header=header[0:-3])
+                        final_img.writeto('/gehme/projects/2020_gcs_with_ml/data/corona_back_database/'+df.name+'/'+os.path.basename(path_1h),overwrite=True) 
 
-        #         final_img.writeto('/gehme/projects/2020_gcs_with_ml/data/corona_back_database/'+df.name+'/'+os.path.basename(path_1h),overwrite=True) 
+
+                except ValueError as e:
+                    print("error "+str(e)+" on "+path_1h+"  or  "+path_2h)
 
     paths=pd.DataFrame(paths,columns=['paths'])
     paths = paths.drop_duplicates()
@@ -58,7 +73,7 @@ def pathlist(df):
 
 
 
-pathlist(lasco)
+pathlist(cor2)
 
 
 
