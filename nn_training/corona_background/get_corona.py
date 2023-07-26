2f6b2c85d0cef2136bbf598c3e986cc38b82c1f9
Florencia Cisterna <f.cisterna@alumno.um.edu.ar>
2023-07-26 15:09:39 -0300

    corrected errors in the imsize of the backgrouns

--- nn_training/corona_background/get_corona.py
+++ nn_training/corona_background/get_corona.py
@@ -34,7 +34,7 @@ def get_corona(sat, imsize=None, diff=True, rnd_rot=False):
     #files
     
     
-    cor2_path="/gehme/projects/2020_gcs_with_ml/data/corona_back_database/cor2"
+    cor2_path= "/gehme/projects/2020_gcs_with_ml/data/corona_background_affects/cor2"#/gehme/projects/2020_gcs_with_ml/data/corona_background_kincat/cor2"
     lasco_path="/gehme/projects/2020_gcs_with_ml/data/corona_back_database/lasco"
     size_occ=[2.6, 3.7, 2]# Occulters size for [sat1, sat2 ,sat3] in [Rsun] 3.7
 
@@ -58,43 +58,15 @@ def get_corona(sat, imsize=None, diff=True, rnd_rot=False):
     
     print('Using back file ', p0)
     oimg= fits.open(p0)[0].data
-    #i0, h0 = sunpy.io._fits.read(p0)[0]
     h0= fits.getheader(p0)
-    #oimg=i0
-
-    
-    #breakpoint() # THERE IS AN ERROR IN THE HEADERS OF cor2_path FILES THAT WE CREATED, WE NEED TO FIX IT
-
-    # # use fixed file
-    data_path = '/gehme/data/stereo/secchi/L1'
-    #p0 = data_path + '/a/img/cor2/20110319/20110319_110800_14c2A.fts'
-    p0 = data_path + '/a/img/cor2/20110319/20110319_120800_14c2A.fts'
-    #oimg, h0 = sunpy.io._fits.read(p0)[0]
-    # p1img, _ = sunpy.io._fits.read(p1)[0]    
-    # oimg = p1img - p0img
-    
-    p1="/gehme/projects/2020_gcs_with_ml/data/corona_back_database/cor2/cor2_a/20110319_120800_14c2A.fts.fits"
-    oimg0= fits.open(p0)[0].data
-    h0= fits.getheader(p0)
-    oimg1= fits.open(p1)[0].data
-    h1= fits.getheader(p1)
-   
-    h1['NAXIS1'] = 1024   
-    h1['NAXIS2'] = 1024
-
 
     if rnd_rot:
         oimg = scipy.ndimage.rotate(oimg, np.random.randint(low=0, high=360), reshape=False)
  
-    #if imsize is not None:
-        #oimg0 = rebin(oimg,imsize,operation='mean') 
-        #h0['NAXIS1'] = imsize[0]   
-        #h0['NAXIS2'] = imsize[1]
-
-
-
-    #breakpoint()    
-    return oimg1, h1, size_occ[sat]
+    if imsize is not None:
+        oimg = rebin(oimg,imsize,operation='mean') 
+   
+    return oimg, h0, size_occ[sat]
 
 
     #sattelite positions
