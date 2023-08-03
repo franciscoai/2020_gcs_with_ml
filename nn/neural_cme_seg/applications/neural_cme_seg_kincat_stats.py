718888a5709326b76de264f7b685c382336c0d9e
Florencia <florenciacisterna14@gmail.com>
2023-08-02 21:07:59 -0300

    updated neural_cme_seg_kinct_stats.py, not working yet

--- nn/neural_cme_seg/applications/neural_cme_seg_kincat_stats.py
+++ nn/neural_cme_seg/applications/neural_cme_seg_kincat_stats.py
@@ -4,6 +4,7 @@ def read_fits(file_path,smooth_kernel=[0,0]):
     try:       
         
         file=glob.glob((file_path)[0:-5]+"*")
+        file = [r for r in file if r.endswith((".fits", ".fts"))]
         img = fits.open(file[0])
         img=(img[0].data).astype("float32")
         #img = gaussian_filter(img, sigma=smooth_kernel)
@@ -17,47 +18,48 @@ def read_fits(file_path,smooth_kernel=[0,0]):
         return None
     
 def rec2pol(mask,scores,labels,boxes,imsize):
-    
+    pol_mask=[]
+    pts=[]
     mask_threshold = 0.6 # value to consider a pixel belongs to the object
     scr_threshold = 0.6 # only detections with score larger than this value are considered
     nans = np.full(imsize, np.nan)
     try:
         if boxes is not None:
-            nb = 0
             for b in boxes:
                 if scores is not None:
-                    scr = scores[nb]
+                    scr = scores[0]
                 else:
                     scr = 0   
                 if scr > scr_threshold:             
                     masked = nans.copy()
-                    masked[:, :][mask[nb] > mask_threshold] = nb
-                nb+=1
+                    masked[:, :][mask[0] > mask_threshold] = 0
+                    
 
-                height, width = masked.shape
-                # Calcula el centro en cada dimensión
-                center_x = width // 2
-                center_y = height // 2
-                pol_mask=[]
-                center=[center_x,center_y]
-                for x in range(width):
-                    for y in range(height):
-                        value=masked[x,y]
-                        if not np.isnan(value):
-                            x_dist = (center_x-x)
-                            y_dist = (center_y-y)
-                            distance= math.sqrt(x_dist**2 + y_dist**2)
-                            angle=math.atan(x_dist / y_dist)
-                            angle_deg = math.degrees(angle)
-                            pol_mask.append([distance,angle_deg])
+                    height, width = masked.shape
+                    # Calcula el centro en cada dimensión
+                    center_x = width // 2
+                    center_y = height // 2
+                    
+                    center=[center_x,center_y]
+                    for x in range(width):
+                        for y in range(height):
+                            value=masked[x,y]
+                            if not np.isnan(value):
+                                pts.append([x,y])
+                                x_dist = (center_x-x)
+                                y_dist = (center_y-y)
+                                distance= math.sqrt(x_dist**2 + y_dist**2)
+                                angle=np.arctan2(y_dist,x_dist)
+                                
+                                pol_mask.append([distance,angle])
                 
-        return pol_mask,center
+        return pol_mask,center,masked,pts
     except:
         print("A parameter its None")
 
 
 
-def plot_to_png(ofile,orig_img, masks, pol_mask, center,imsize, title=None, labels=None, boxes=None, scores=None):
+def plot_to_png(ofile,pts,orig_img, masks, pol_mask, center,imsize, title=None, labels=None, boxes=None, scores=None):
     """
     Plot the input images (orig_img) along with the infered masks, labels and scores
     in a single image saved to ofile
@@ -74,73 +76,63 @@ def plot_to_png(ofile,orig_img, masks, pol_mask, center,imsize, title=None, labe
     angles = [s[1] for s in pol_mask]
     ang=[]
     points=[]
-    min_ang = np.radians(min(angles))
-    max_ang = np.radians(max(angles))
-    cpa_ang=np.radians((max_ang-min_ang)/2)
+    min_ang = min(angles)
+    max_ang = max(angles)
+    idx_min = angles.index(min_ang)
+    idx_max = angles.index(max_ang)
+    pts_min=pts[idx_min]
+    pts_max=pts[idx_max]
+    cpa_ang=(max_ang-min_ang)/2
     ang.append([min_ang,max_ang,cpa_ang])
+    angulo_max=np.degrees(max_ang)
+    angulo_min=np.degrees(min_ang)
+    angulo_cpa=np.degrees(cpa_ang)
+    title=str(angulo_max)+"/"+str(angulo_cpa)+"/"+str(angulo_min)
     for i in ang[0]:
         vector=np.array([np.cos(i), np.sin(i)])
         start_point = center
-        end_point = start_point + imsize[0] * vector
-        x_points = np.array([start_point[0], end_point[0]])
-        y_points = np.array([start_point[1], end_point[1]])
+        x_end = start_point[0] + (imsize[0]/2) * vector[0]
+        y_end = start_point[1] + (imsize[1]/2) * vector[1]
+        x_points = np.array([start_point[0], x_end])
+        y_points = np.array([start_point[1], y_end])
         points.append([x_points,y_points])
 
-
-
+    for i in range(len(orig_img)):
         fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-
-        # Plotear la primera imagen en el primer subplot
-        axs[0].imshow(orig_img, cmap='gray')
-        axs[0].set_title('Imagen 1')
+        axs[0].imshow(orig_img[0], vmin=0, vmax=1, cmap='gray')
         axs[0].axis('off')
-
-        # Plotear la segunda imagen en el segundo subplot
-        axs[1].imshow(orig_img, cmap='gray')
-        axs[1].set_title('Imagen 2')
-        axs[1].axis('off')
-
-        # Ajustar el espacio entre los subplots
-        plt.tight_layout()
-
-        # Guardar la figura como una imagen
-        plt.savefig(opath)
-
-
-    #for i in range(len(orig_img)):
-        #axs[i].imshow(orig_img[i], vmin=0, vmax=1, cmap='gray')
-        #axs[i].axis('off')
-        #axs[i+1].imshow(orig_img[i], vmin=0, vmax=1, cmap='gray')        
-        #axs[i+1].axis('off')        
-        #if boxes is not None:
-            #nb = 0
-            #for b in boxes[i]:
-                #if scores is not None:
-                #    scr = scores[i][nb]
-                #else:
-                #    scr = 0   
-                #if scr > scr_threshold:             
-                    #masked = nans.copy()
-                    #masked[:, :][masks[i][nb] > mask_threshold] = nb              
-                    #axs[i+1].imshow(masked, cmap=cmap, alpha=0.4, vmin=0, vmax=len(color)-1) # add mask
+        axs[1].imshow(orig_img[0], vmin=0, vmax=1, cmap='gray')      
+        axs[1].axis('off')        
+        if boxes is not None:
+            nb = 0
+            for b in boxes[i]:
+                if scores is not None:
+                    scr = scores[i][nb]
+                else:
+                    scr = 0   
+                if scr > scr_threshold:             
+                    masked = nans.copy()
+                    masked[:, :][masks[i][nb] > mask_threshold] = nb              
+                    axs[i+1].imshow(masked, cmap=cmap, alpha=0.4, vmin=0, vmax=len(color)-1) # add mask
        
-                    #axs[i+1].plot(points[0][0], points[0][1], color='blue', label='Recta')
-                    #axs[i+1].plot(points[1][0],points[1][1] , color='blue', label='Recta')
-                    #axs[i+1].plot(points[2][0], points[2][1], color='blue', label='Recta')
-             
+                    axs[i+1].plot(points[0][0], points[0][1], color='blue', label='Recta')
+                    axs[i+1].plot(points[1][0],points[1][1] , color='orange', label='Recta')
+                    axs[i+1].plot(points[2][0], points[2][1], color='green', label='Recta')
+                    axs[i+1].plot(pts_min[0],pts_min[1] , 'purple')
+                    axs[i+1].plot(pts_max[0],pts_max[1] , 'purple')
                     # Ajustar límites del eje y aspecto
                     #axs[i+1].set_xlim(0, imsize[0])
                     #axs[i+1].set_ylim(0, imsize[1])
-                    #axs[i+1].set_aspect('equal', adjustable='box')
-
-
-                    #if labels is not None:
-                    #    axs[i+1].annotate(obj_labels[labels[i][nb]]+':'+'{:.2f}'.format(scr),xy=b[0:2], fontsize=15, color=color[nb])
+                    #axs[i].set_xlim(0, imsize[0])
+                    #axs[i].set_ylim(0, imsize[1])
+                    if labels is not None:
+                        axs[i+1].annotate(obj_labels[labels[i][nb]]+':'+'{:.2f}'.format(scr),xy=b[0:2], fontsize=15, color=color[nb])
                 #nb+=1 
-    #plt.tight_layout()
-    #plt.show()
-    #plt.savefig(ofile)
-    #plt.close()
+
+    fig.suptitle(title)
+    plt.tight_layout()
+    plt.savefig(ofile)
+    plt.close()
 
 
 
@@ -170,7 +162,8 @@ for folder in folders:
     for file in files:
         if file.endswith(".pkl"):
             print("processing file "+str(file))
-            img_path=image_dir+str(file[:-24])+"/"+file[:-11]+".fts"
+            img_path=data_dir+folder+"/"+str(file[:-11]+".fts")
+            
             pkl_path=data_dir+folder+"/"+file
             with open(pkl_path, 'rb') as archivo_pickle:
                 mask = pickle.load(archivo_pickle)
@@ -178,10 +171,23 @@ for folder in folders:
                 labels= pickle.load(archivo_pickle)
                 boxes= pickle.load(archivo_pickle)
 
-            pol_mask,center=rec2pol(mask,scores,labels,boxes,imsize=imsize)
-            img=read_fits(img_path)
-            f=file[:-11]
-            opath="/gehme-gpu/projects/2020_gcs_with_ml/repo_flor/2020_gcs_with_ml/nn/neural_cme_seg/applications/img/"+folder+str(f)+"_stats.png"#data_dir+folder+"/"+str(f)+"_stats.png"
-
-            plot_to_png(opath,[img],[mask], title=[f],labels=[labels], boxes=[boxes], scores=[scores],pol_mask=pol_mask, center=center,imsize=imsize)
+            pol=rec2pol(mask,scores,labels,boxes,imsize=imsize)
             
+            if (pol is not None) :
+                pol_mask=pol[0]
+                center= pol[1]
+                masked=pol[2]
+                pts=pol[3]
+                f=file[:-11]
+                path="/gehme-gpu/projects/2020_gcs_with_ml/repo_flor/2020_gcs_with_ml/nn/neural_cme_seg/applications/img/"+folder+str(f)+"_mask.png"
+                fig, ax = plt.subplots()
+
+                # Mostrar la imagen en el eje
+                ax.imshow(masked, cmap='gray')
+                fig.savefig(path)
+                img=read_fits(img_path)
+                
+                opath="/gehme-gpu/projects/2020_gcs_with_ml/repo_flor/2020_gcs_with_ml/nn/neural_cme_seg/applications/img/"+folder+str(f)+"_stats.png"#data_dir+folder+"/"+str(f)+"_stats.png"
+                
+                plot_to_png(opath,pts,[img],[mask], title=[f],labels=[labels], boxes=[boxes], scores=[scores],pol_mask=pol_mask, center=center,imsize=imsize)
+                
