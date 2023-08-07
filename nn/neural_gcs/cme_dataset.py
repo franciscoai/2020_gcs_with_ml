Bugfix in loss (gradient not computing) and dataset index

nn/neural_gcs/cme_dataset.py
@@ -25,9 +25,10 @@ class CmeDataset(Dataset):
     def __get_dirs(self, root_dir):
         imgs = []
         dirs = os.listdir(root_dir)
-        dirs = [d for d in dirs if not d.endswith('.csv')]
+        dirs = [int(d) for d in dirs if not d.endswith('.csv')]
+        dirs.sort()
         for d in dirs:
-            imgs.append(os.path.join(root_dir, d))
+            imgs.append(os.path.join(root_dir, str(d)))
         return imgs
     
     def __len__(self):
@@ -38,9 +39,9 @@ class CmeDataset(Dataset):
         mask_dir = os.path.join(self.imgs[idx], 'mask')
         img = read_image(os.path.join(self.imgs[idx], file[0]), mode=torchvision.io.image.ImageReadMode.RGB)
         mask = read_image(os.path.join(mask_dir, '2.png'), mode=torchvision.io.image.ImageReadMode.GRAY)
-        img[0,:,:] = mask
-        img[1,:,:] = mask
-        img[2,:,:] = mask
+        # img[0,:,:] = mask
+        # img[1,:,:] = mask
+        # img[2,:,:] = mask
         img = img.float()
         img = self.__normalize(img)
         img = self.transform(img)
@@ -65,9 +66,6 @@ class CmeDataset(Dataset):
         occulter_mask = torch.squeeze(occulter_mask)
         satpos = torch.squeeze(satpos)
         plotranges = torch.squeeze(plotranges)
-
-        
-
 
         return img, parameters, mask, occulter_mask, satpos, plotranges
 
@@ -96,4 +94,3 @@ class CmeDataset(Dataset):
     #     torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     # ])
         return transform
-    

nn/neural_gcs/mlp_resnet_model.py
@@ -3,46 +3,50 @@ import torchvision.models.detection as detection
 
 
 class Mlp_Resnet(torch.nn.Module):
-    def __init__(self, backbone, input_size=1000, hidden_size=512, output_size=6, gcs_par_rng=None) -> None:
+    def __init__(self, backbone, input_size=1000, hidden_layer=[512, 256, 128, 16], output_size=6, gcs_par_rng=None) -> None:
         super(Mlp_Resnet, self).__init__()
         self.gcs_par_rng = gcs_par_rng
-        self.backbone = backbone
-        # regression head
-        self.regression = torch.nn.Sequential(
-            torch.nn.Linear(input_size, hidden_size),
-            torch.nn.LeakyReLU(),
-            torch.nn.Linear(hidden_size, output_size*8),
-            torch.nn.LeakyReLU(),
-            torch.nn.Linear(output_size*8, output_size)
-        )
+        #self.backbone = backbone
+        self.input_layer = torch.nn.Linear(input_size, hidden_layer[0])
+        self.output_layer = torch.nn.Linear(hidden_layer[-1], output_size)
+        self.hidden_layer = self._generateHiddenLayer(hidden_layer)        
         #self.backbone = torch.nn.Sequential(*(list(backbone.children())[:-1])) # remove last layer
 
         # freeze backbone parameters
-        for param in self.backbone.parameters():
-            param.requires_grad = False
+        # for param in self.backbone.parameters():
+        #     param.requires_grad = False
 
         # Unfreeze last 2 conv layers and the linear layer
-        for param in self.backbone.layer4.parameters():
-            param.requires_grad = True
+        # for param in self.backbone.layer4.parameters():
+        #     param.requires_grad = True
 
         
         
 
     def forward(self, x):
-        freatures = self.backbone(x)
+        #freatures = self.backbone(x)
         #freatures = freatures.view(freatures.size(0), -1)
-        predictions = self.regression(freatures)
+        predictions = self.input_layer(x)
+        predictions = self.hidden_layer(predictions)
+        predictions = self.output_layer(predictions)
         # for i in range(predictions.shape[0]):
         #     for j in range(predictions.shape[1]):
         #         if j in [0,1,2]:
         #             predictions[i][j] = torch.tanh(predictions[i][j]) * torch.max(torch.abs(self.gcs_par_rng[j,:]))
         #         else:
         #             predictions[i][j] = torch.sigmoid(predictions[i][j]) * torch.max(torch.abs(self.gcs_par_rng[j,:]))
-        for i in range(predictions.shape[0]):
-            for j in range(predictions.shape[1]):
-                if j in [0,1,2]:
-                    #predictions[i][j] += 20 #torch.tanh(predictions[i][j]) * torch.max(torch.abs(self.gcs_par_rng[j,:])) 
-                    continue
-                else:
-                    predictions[i][j] += self.gcs_par_rng[j,0]
+        # for i in range(predictions.shape[0]):
+        #     for j in range(predictions.shape[1]):
+        #         if j in [0,1,2]:
+        #             #predictions[i][j] += 20 #torch.tanh(predictions[i][j]) * torch.max(torch.abs(self.gcs_par_rng[j,:])) 
+        #             continue
+        #         else:
+        #             predictions[i][j] += self.gcs_par_rng[j,0]
         return predictions
+
+    def _generateHiddenLayer(self, hidden_layer):
+        layers = []
+        for i in range(len(hidden_layer)-1):
+            layers.append(torch.nn.Linear(hidden_layer[i], hidden_layer[i+1]))
+            layers.append(torch.nn.LeakyReLU())
+        return torch.nn.Sequential(*layers)