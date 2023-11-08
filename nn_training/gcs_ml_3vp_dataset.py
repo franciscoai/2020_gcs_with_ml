98c581b64ec54383776ded502c32a25a12113851
AgnidDrage <marianosanchestoledo@gmail.com>
2023-11-08 12:51:48 -0300

    refractor done and fixes in images

--- nn_training/gcs_ml_3vp_dataset.py
+++ nn_training/gcs_ml_3vp_dataset.py
@@ -29,10 +29,12 @@ def get_params(par_names, par_rng): #par_names = ['CMElon', 'CMElat', 'CMEtilt',
                          #            [[-180,180],[-70,70],[-90,90],[5,10],[0.2,0.6],[10,60],[7e2,1e3]]
     params = []
     for i in range(len(par_names[0:-2])):
-        params.append(np.random.uniform(low=par_rng[i][0], high=par_rng[i][1]))
-    if params in used_params:
-        params = get_params(par_names, par_rng)
+        params.append(random_driver.uniform(low=par_rng[i][0], high=par_rng[i][1]))
+    
+    # matching = check_df.loc[check_df[] == params]
+
     return params
+
 
 def save_png(array, ofile=None, range=None):
     '''
@@ -76,9 +78,10 @@ def save_cases(succesful_cases, otype, iter_counter):
             ofile = mask_folder +'/sat{}.png'.format(i+1)
             fig=save_png(cme_mask,ofile=ofile, range=[0, 1])
             # mask for occulter
-            arr = succesful_cases[i][1]
+            occ_mask = succesful_cases[i][1]
             ofile = mask_folder +'/sat{}_occ.png'.format(i+1)
-            fig=save_png(arr,ofile=ofile, range=[0, 1])
+            fig=save_png(occ_mask,ofile=ofile, range=[0, 1])
+            # total intensity image
             if synth_int_image:
                 btot = succesful_cases[i][2]
                 m = np.mean(btot)
@@ -92,10 +95,18 @@ def save_cases(succesful_cases, otype, iter_counter):
             mask = succesful_cases[i][0]
             ofile = mask_folder +'/sat{}.png'.format(i+1)
             fig=save_png(mask,ofile=ofile, range=[0, 1])
+            # cloud mesh
             arr_cloud = succesful_cases[i][1]
             ofile = folder +'/{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_{:08.3f}_sat{}_mesh.png'.format(
                 params[0], params[1], params[2], params[3], params[4], params[5], i+1)
             fig=save_png(arr_cloud,ofile=ofile, range=[0, 1])
+            # occ mask
+            occ_mask = succesful_cases[i][2]
+            ofile = mask_folder +'/sat{}_occ.png'.format(i+1)
+            fig=save_png(occ_mask,ofile=ofile, range=[0, 1])
+
+def save_to_csv(df, config_file_name):
+    df.to_csv(config_file_name)
 
 def checkFailure(failure_counter, n_sat, min_nviews):
     if failure_counter > n_sat - min_nviews:
@@ -108,14 +119,13 @@ def pygcsWrapper(params, satpos):
 def raytracewcsWrapper(header, params, imsize, occrad, in_sig, out_sig, nel):
     return rtraytracewcs(header, params[0], params[1], params[2], params[3], params[4], params[5], imsize=imsize, occrad=occrad, in_sig=in_sig, out_sig=out_sig, nel=nel)
 
-
 ######Main
 
 # CONSTANTS
 #files
 DATA_PATH = '/gehme/data'
-OPATH = '/gehme-gpu/projects/2020_gcs_with_ml/data/gcs_ml_3VP' #'/gehme/projects/2020_gcs_with_ml/data/cme_seg_training'
-OVERWRITE = True # set to True to overwrite existing files
+OPATH = '/gehme-gpu/projects/2020_gcs_with_ml/data/gcs_ml_3VP_test' #'/gehme/projects/2020_gcs_with_ml/data/cme_seg_training'
+OVERWRITE = False # set to True to overwrite existing files
 n_sat = 3 #number of satellites to  use [Cor2 A, Cor2 B, Lasco C2]
 min_nviews = 3 # minimum number succesful views 
 
@@ -124,21 +134,21 @@ min_nviews = 3 # minimum number succesful views
 # level_cme: CME intensity level relative to the mean background corona
 par_names = ['CMElon', 'CMElat', 'CMEtilt', 'height', 'k','ang', 'level_cme', 'satpos', 'plotranges'] # par names
 par_units = ['deg', 'deg', 'deg', 'Rsun','','deg',''] # par units
-par_rng = [[-180,180],[-70,70],[-90,90],[3,10],[0.2,0.6],[10,60],[7e2,1e3]] # min-max ranges of each parameter in par_names {2.5,7} heights
-par_num = int(1e5)  # total number of samples that will be generated for each param (there are nsat images per param combination)
+par_rng = [[-180,180],[-70,70],[-90,90],[3,10],[0.2,0.6],[10,60],[1e-1,1e1]] # min-max ranges of each parameter in par_names {2.5,7} heights
+par_num = int(50)  # total number of samples that will be generated for each param (there are nsat images per param combination)
 rnd_par=False # when true it apllies a fixed seed to generate the same parameters for each run
 seed = 47 # seed to use when rnd_par is True
 same_corona=True # Set to True use a single corona back for all par_num cases
 same_position=True # Set to True to use the same set of satteite positions(not necesarly the same background image)
 
 # Syntethic image options
-synth_int_image=False # set to True to generate a synthetic image with a cme
+synth_int_image=True # set to True to generate a synthetic image with a cme
 plot_mask_rtc_only=False # set to True to plot only the mask from raytracing, synth_int_image must be True
 imsize=np.array([512, 512], dtype='int32') # output image size
 level_occ=0. #mean level of the occulter relative to the background level
 cme_noise= [0,2.] #gaussian noise level of cme image. [mean, sd], both expressed in fractions of the cme-only image mean level. Set mean to None to avoid
-occ_noise = [0,30.] # occulter gaussian noise. [mean, sd] both expressed in fractions of the abs mean background level. Set mean to None to avoid
-mesh=True # set to also save a png with the GCSmesh (only for otype='png')
+occ_noise = [None,None] # occulter gaussian noise. [mean, sd] both expressed in fractions of the abs mean background level. Set mean to None to avoid
+mesh=False # set to also save a png with the GCSmesh (only for otype='png')
 otype="png" # set the ouput file type: 'png' or 'fits'
 im_range=2. # range of the color scale of the output final syntethyc image in std dev around the mean
 back_rnd_rot=False # set to randomly rotate the background image around its center
@@ -148,20 +158,46 @@ two_cmes = False # set to include two cme per image on some (random) cases
 
 
 # initialize vars
-used_params = []
-iter_counter = -1
+iter_counter = 0
 if not rnd_par:
-    np.random.seed(seed)
+    random_driver = np.random.RandomState(seed)
+else:
+    seed = np.random.randint(0, int(1e5))
+    random_driver = np.random.RandomState(seed)
+    
 
 OPATH = OPATH + "_size_" + str(par_num) + "_seed_" + str(seed)
-if os.path.exists(OPATH) and OVERWRITE:
-    os.system("rm -r " + OPATH)
-os.makedirs(OPATH, exist_ok=True)
 
 # Save configuration to .CSV
 date_str = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d_')
 configfile_name = OPATH + '/' + date_str+'Set_Parameters.csv'
+faileddf_name = OPATH + '/' + date_str+'Failed_Parameters.csv'
 succesful_df = pd.DataFrame(columns=par_names)
+failed_df = pd.DataFrame(columns=par_names)
+check_df = succesful_df.iloc[:, :-2]
+used_params = set()
+
+if os.path.exists(OPATH) and OVERWRITE:
+    os.system("rm -r " + OPATH)
+
+elif os.path.exists(OPATH) and not OVERWRITE:
+    #check if the csv file exists
+    list_files = os.listdir(OPATH)
+    csv_file = [s for s in list_files if "Set_Parameters.csv" in s][0]
+    failed_csv_file = [s for s in list_files if "Failed_Parameters.csv" in s][0]
+    configfile_name = OPATH + '/' + csv_file
+    faileddf_name = OPATH + '/' + failed_csv_file
+    succesful_df = pd.read_csv(configfile_name, index_col=0)
+    failed_df = pd.read_csv(faileddf_name, index_col=0)
+    iter_counter = len(succesful_df.index)
+    total_iter = iter_counter + len(failed_df.index)
+    #iterate the np.random to the last iteration
+    for i in range(total_iter):
+        temp = get_params(par_names, par_rng)
+        print(temp)
+
+os.makedirs(OPATH, exist_ok=True)
+
 
 # defining lists
 satpos_all = []
@@ -225,7 +261,7 @@ while iter_counter != par_num:
             
 
     for sat in range(n_sat):
-        print(f'Generating image {sat+1} of {n_sat} for iteration {iter_counter+1} of {par_num}')
+        print(f'Generating image {sat+1} of {n_sat} for iteration {iter_counter} of {par_num}')
         if too_many_failures:
             break
         #defining ranges and radius of the occulter
@@ -249,7 +285,6 @@ while iter_counter != par_num:
                 print(f'WARNING: CME raytracing did not work')
                 failure_counter += 1
                 too_many_failures = checkFailure(failure_counter, n_sat, min_nviews)
-                breakpoint()
                 break
             mask = get_cme_mask(btot_mask,inner_cme=inner_hole_mask)          
             mask_npix= len(mask[mask>0].flatten())
@@ -268,8 +303,8 @@ while iter_counter != par_num:
             break
 
         #mask for occulter
-        arr = np.zeros(xx.shape)
-        arr[r <= size_occ[sat]] = 1
+        occ_mask = np.zeros(xx.shape)
+        occ_mask[r <= size_occ[sat]] = 1
 
         #Total intensity (Btot) figure from raytrace:
         if synth_int_image:  
@@ -287,12 +322,14 @@ while iter_counter != par_num:
             back = back_corona[sat]
             if back_rnd_rot:
                 back =  scipy.ndimage.rotate(back, np.random.randint(low=0, high=360), reshape=False)
-            level_back = np.mean(back)
+            back_mean = np.mean(back)
+            back_std = np.std(back)
+        
 
             #cme
             #adds a random patchy spatial variation of the cme only 
             var_map = low_freq_map(dim=[imsize[0],imsize[1],1],off=[1],var=[1.5],func=[13])
-            btot = var_map*(btot/np.max(btot))*params[6]*level_back
+            btot = var_map*(btot/np.max(np.abs(btot))) * params[6] * back_std + back_mean
             #adds noise
             if cme_noise[0] is not None :
                 m = np.mean(btot)
@@ -318,10 +355,10 @@ while iter_counter != par_num:
                 btot = np.array(btot_mask)
             #adds occulter
             if occ_noise[0] is not None:
-                noise=np.random.normal(loc=occ_noise[0]*level_back, scale=occ_noise[1]*np.abs(level_back), size=imsize)[r <= size_occ[sat]]
+                noise=np.random.normal(loc=occ_noise[0]*back_mean, scale=occ_noise[1]*np.abs(back_mean), size=imsize)[r <= size_occ[sat]]
             else:
                 noise =0        
-            btot[r <= size_occ[sat]] = level_occ*level_back + noise
+            btot[r <= size_occ[sat]] = level_occ*back_mean + noise
         
         case_stuff =[]
         if otype=="fits":
@@ -330,11 +367,11 @@ while iter_counter != par_num:
             if synth_int_image:
                 cme = fits.PrimaryHDU(btot)
                 case_stuff.append(cme)
-            occ = fits.PrimaryHDU(arr)
+            occ = fits.PrimaryHDU(occ_mask)
             case_stuff.append(occ)
         elif not mesh and otype == "png":
             case_stuff.append(mask)
-            case_stuff.append(arr)
+            case_stuff.append(occ_mask)
             if synth_int_image:
                 case_stuff.append(btot)
         elif mesh and otype=='png':
@@ -344,22 +381,32 @@ while iter_counter != par_num:
             arr_cloud=pnt2arr(x,y,plotranges,imsize, sat)
             case_stuff.append(mask)
             case_stuff.append(arr_cloud)
+            case_stuff.append(occ_mask)
         
         successful_cases.append(case_stuff)
         success_counter += 1
 
     if success_counter >= min_nviews:
 
-        iter_counter += 1
-
         # save cases
         save_cases(successful_cases, otype, iter_counter)
 
-        used_params.append(params)
         params.append(satpos)
         params.append(plotranges)
         succesful_df.loc[iter_counter] = params
 
-#save to csv
-succesful_df.to_csv(configfile_name)
+        # save to csv
+        save_to_csv(succesful_df, configfile_name)
 
+        iter_counter += 1
+
+    else:
+        params.append(satpos)
+        params.append(plotranges)
+        try:
+            failed_df.loc[iter_counter] = params
+        except:
+            array_of_objects = np.asarray(params, dtype="object") # Fix a weird pandas bug
+            failed_df.loc[iter_counter] = array_of_objects
+
+        save_to_csv(failed_df, faileddf_name)
