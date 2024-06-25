Fixes in language and img rotation when plot

nn/neural_gcs/sirats_utils/sirats_plotter.py
@@ -55,7 +55,7 @@ class SiratsPlotter:
         plotranges = plotranges.cpu().detach().numpy()
 
         # flip images in y axis
-        images = np.flip(images, axis=1)
+        # images = np.flip(images, axis=1)
 
         fig, ax = plt.subplots(1, 3, figsize=(10, 5))
         fig.tight_layout()
@@ -75,6 +75,9 @@ class SiratsPlotter:
                                   ncirc=100, ncross=100)
             x, y = clouds[0, :, 1], clouds[0, :, 2]
             arr_cloud = pnt2arr(x, y, [plotranges[i, :]], images.shape[1:3], 0)
+
+            # flip arr_cloud in y axis
+            arr_cloud = np.flip(arr_cloud, axis=0)
 
             ax[i].imshow(images[i, :, :], cmap="gray", vmin=0, vmax=1)
             ax[i].imshow(arr_cloud, cmap='Greens', alpha=0.6,