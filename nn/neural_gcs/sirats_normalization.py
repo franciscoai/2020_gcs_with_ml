SIRATS real img normalization fixed

nn/neural_gcs/sirats_normalization.py
@@ -11,24 +11,37 @@ def binary_mask_normalization(img: torch.Tensor):
     return img
 
 def real_img_normalization(img: torch.Tensor, excl_occulter_level = None):
+    """
+    Normalize the input image tensor using Sirats normalization method.
+
+    Args:
+        img (torch.Tensor): The input image tensor. Should have shape [3, x, y].
+        excl_occulter_level (int or str, optional): The value to exclude from normalization. 
+            If 'auto', the most frequent integer value in the image will be excluded. 
+            If an integer value is provided, that value and its adjacent value will be excluded. 
+            Defaults to None.
+
+    Returns:
+        torch.Tensor: The normalized image tensor.
+
+    """
     sd_range=1.5
-    # m = torch.mean(img)
-    # sd = torch.std(img)
     #exclude occulter values at excl_occulter_level
-    if excl_occulter_level is not None:
-        if excl_occulter_level == 'auto':
-            # finds the most frequent integer value in the image
-            occ_indx = (img != stats.mode(img.flatten(),keepdims=True)[0][0])
+    for i in range(3):
+        if excl_occulter_level is not None:
+            if excl_occulter_level == 'auto':
+                # finds the most frequent integer value in the image
+                occ_indx = (img[i] != stats.mode(img[i].flatten(),keepdims=True)[0][0])
+            else:
+                occ_indx = (img[i] != excl_occulter_level) & (img[i] != excl_occulter_level-1)
+            m = np.nanmean(img[i][occ_indx])
+            sd = np.nanstd(img[i][occ_indx])
         else:
-            occ_indx = (img != excl_occulter_level) & (img != excl_occulter_level-1)
-        m = np.nanmean(img[occ_indx])
-        sd = np.nanstd(img[occ_indx])
-    else:
-        m = np.nanmean(img)
-        sd = np.nanstd(img)
-    img = (img - m + sd_range * sd) / (2 * sd_range * sd)
-    img[img >1]=1
-    img[img <0]=0
+            m = np.nanmean(img[i])
+            sd = np.nanstd(img[i])
+        img[i] = (img[i] - m + sd_range * sd) / (2 * sd_range * sd)
+        img[i][img[i] >1]=1
+        img[i][img[i] <0]=0
     return img
 
 def normalize(self, image, excl_occulter_level='auto', sd_range=2, norm_limits=[None, None], increase_contrast=False, 

nn/neural_gcs/sirats_run.py
@@ -333,23 +333,19 @@ def main(configuration: Configuration):
                     # Add occulter to images
                     center_idxs = [
                         ((case.shape[0] - 1) // 2, (case.shape[1] - 1) // 2) for case in case_list]
+                    
                     # Occulters size for [sat1, sat2 ,sat3] in [Rsun]
-                    occulter_size = [3., 3., 2.4]
+                    occulter_size = [3., 3., 2.4] # [0.3, 0.3, 2.4]
+
                     # occ_center=[(30,-15),(0,-5),(0,0)] # [(38,-15),(0,-5),(0,0)] # (y,x)
                     r_values = [radius_to_px(
                         plotranges[i], case_list[i].shape, event_headers[i], i) for i in range(len(occulter_size))]
+                    
                     # case_list = [add_occulter(case, occulter_size[i], center_idxs[i]) for i, case in enumerate(case_list)]
                     for i in range(len(case_list)):
                         case = case_list[i]
                         case[r_values[i] <= occulter_size[i]] = 0
                         case_list[i] = case
-
-
-                    # Using synth images for testing
-                    for iteration, (img, targets, sat_masks, occulter_masks, satpos, plotranges, idx) in enumerate(cme_test_dataloader, 0):
-                        case = img[8] # Testing case 9
-                        case_list = [case[0], case[1], case[2]]
-                        break
 
                     # Resize event images
                     if case_list[0].shape[0] != IMG_SIZE[0] or case_list[0].shape[1] != IMG_SIZE[1]:
@@ -369,6 +365,9 @@ def main(configuration: Configuration):
 
                     # join event images
                     event_img = torch.stack(case_list, dim=0)
+
+                    # Normalize event images
+                    event_img = real_img_normalization(event_img)
 
                     # Add batch dimension
                     event_img = event_img.unsqueeze(0)

nn/neural_gcs/sirats_utils/sirats_plotter.py
@@ -216,7 +216,7 @@ class SiratsPlotter:
             ", ".join([f"{x:.2f}" for x in plotranges[1, :]])} -- fixed_plotranges: {", ".join([f"{x:.2f}" for x in fixed_plotranges[1, :]])}\n\n'
         suptitle += f'lasco satpos: {", ".join([f"{x:.2f}" for x in satpos[2, :]])} -- fixed_satpos: {", ".join([f"{x:.2f}" for x in fixed_satpos[2, :]])} -- plotranges: {
             ", ".join([f"{x:.2f}" for x in plotranges[2, :]])} -- fixed_plotranges: {", ".join([f"{x:.2f}" for x in fixed_plotranges[2, :]])}\n\n'
-        suptitle += f'Using fixed satpos = {use_fixed}'
+        suptitle += f'Using fixed satpos and plotranges = {use_fixed}'
         suptitle += f'\n\nPrediction: {prediction}'
 
         fig.suptitle(suptitle, x=0.05, y=.95, horizontalalignment='left')
@@ -233,6 +233,11 @@ class SiratsPlotter:
                 x, y = clouds[0, :, 1], clouds[0, :, 2]
                 arr_cloud = pnt2arr(x, y, [plotranges[i, :]], IMG_SIZE[1:3], 0)
 
+                # flip arr_cloud in x
+                arr_cloud = np.flip(arr_cloud, axis=0)
+
+                imgs_mean = np.mean(imgs[i, :, :])
+                imgs_std = np.std(imgs[i, :, :])
 
                 ax[i].imshow(imgs[i, :, :], cmap="gray", vmin=0, vmax=1)
                 ax[i].imshow(arr_cloud, cmap='Greens',
@@ -248,7 +253,10 @@ class SiratsPlotter:
                 # flip arr_cloud in x
                 arr_cloud = np.flip(arr_cloud, axis=0)
 
-                ax[i].imshow(imgs[i, :, :], cmap="gray", vmin=0, vmax=1)
+                imgs_mean = np.mean(imgs[i, :, :])
+                imgs_std = np.std(imgs[i, :, :])
+
+                ax[i].imshow(imgs[i, :, :], cmap="gray", vmin=imgs_mean - imgs_std *3, vmax=imgs_mean + imgs_std *3)
                 ax[i].imshow(arr_cloud, cmap='Greens',
                              alpha=0.6, vmin=0, vmax=1)
 